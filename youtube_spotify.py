29830f3dea989c88099034775f68e77c14bf7070
Timmy <46507219+timmarr98@users.noreply.github.com>
2020-05-27 17:19:59 -0400

    Add files via upload

--- youtube_spotify.py
+++ youtube_spotify.py
@@ -7,32 +7,39 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
 import youtube_dl
 
+#initialize the youtube API 
 youtube = build('youtube', 'v3',developerKey = api_key)
 
+#get the request for playlist videos 
 req = youtube.playlistItems().list( # pylint: disable=no-member
     part="snippet",
     playlistId="PLxhnpe8pN3TmtjcQM7zcYAKhQXPdOHS3Y",
     maxResults=20)
 
+#execute the request 
 res = req.execute()
+
 scope = 'playlist-modify-public'
 username = input("Enter your Spotify Username: ")
 
+#pass user name and scope to token 
 token = util.prompt_for_user_token(username, scope)
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+#if the token is valid, continue 
 if token:
     sp = spotipy.Spotify(auth = token)
     playlist = sp.user_playlist_create(username, "Generated Playlist")
     playlist__ID = playlist['id']
     print(playlist__ID)
 
-
+#get the artist and song from 
 def getArtist_Song(x):
-    Songs = set()
-    for item in x['items']:
-        #print(json.dumps(item, indent =4))
+    Songs = set() #Set of non-duplicate artists/songs
+    for item in x['items']: #iterate through the items in the playlist
+      
+        #format the URL 
         videoURI = "https://www.youtube.com/watch?v={}".format(item['snippet']['resourceId']['videoId'])
  
        #To obtain the official song title and artist from the youtube_dl database
@@ -41,9 +48,6 @@ def getArtist_Song(x):
         
         artiste = video["artist"]
         song_name = video["track"]
-        if (artiste != "None" or song_name != "None") :
-         artiste = video["artist"]
-         song_name = video["track"]
         if(artiste is None):
          print("The YouTube database does not have an official Artist name!")
          continue
@@ -60,9 +64,6 @@ def getArtist_Song(x):
             artiste is None or video is None
         if (artiste is not None and video is not None):
          Songs.add("spotify:track:" + song_id)
-        #print(song_id)
-        #add to a playlist
-       # sp.user_playlist_add_tracks(username, playlist__ID,"spotify:track:{song_id} )
     sp.user_playlist_add_tracks(username, playlist__ID, Songs )
 
 getArtist_Song(res)
